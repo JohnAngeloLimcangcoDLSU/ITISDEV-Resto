9119e2b8121dc56d7eee4b79d7ef40c543839daa
John Sarmiento <johnsarmiento117@gmail.com>
2023-04-06 15:13:11 +0800

    Update Header.js

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,19 +1,11 @@
 import React from 'react'
-import '../styles/Header.css'
-import PersonIcon from '@mui/icons-material/Person';
+import './header.css';
 
-function Header() {
-  return (
-    <div className='header'>
-        <div className='headerTitle'>
-            <h1>Resto</h1>
-        </div>
-        <div className='headerUser'>
-            <h2>Manager</h2>
-            <PersonIcon />
-        </div>
-    </div>
-  )
+
+function header(){
+    return <div class = "header">
+    <h1>Header Sample</h1>
+  </div>
 }
 
-export default Header
+export default header
