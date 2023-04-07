10da6d31fcbe0f4468e71fc30887fbf02a055654
JohnAngeloLimcangcoDLSU <john_angelo_limcangco@dlsu.edu.ph>
2023-04-07 16:27:28 +0800

    initial push

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,9 @@ import './App.css';
 import Header from './components/Header'
 import Sidebar_Manager from './components/Sidebar_Manager'
 import MANAGER_viewDish from './pages/MANAGER_viewDish';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import MANAGER_addDish from './pages/MANAGER_addDish';
+import MANAGER_deleteDish from './pages/MANAGER_deleteDish';
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
 function App() {
   return (
@@ -10,7 +12,12 @@ function App() {
       <Router>
         <Header />
         <Sidebar_Manager />
-        <MANAGER_viewDish />
+        <MANAGER_addDish />
+        <Routes> 
+          <Route path="/" exact component={MANAGER_viewDish }/>
+
+        </Routes>
+     
         
       </Router>
     </div>
---
+++ client/src/pages/MANAGER_addDish.js
@@ -0,0 +1,90 @@
+import React from 'react'
+import '../styles/MANAGER_addDish.css'
+
+function MANAGER_addDish() {
+
+    // Get the "Add Ingredient" button and the ingredient row
+    const addRowBtn = document.querySelector(".add-row-btn");
+    const ingredientRow = document.querySelector(".ingredient-row");
+  
+    // Add event listener to "Add Ingredient" button
+    addRowBtn.addEventListener("click", () => {
+      // Create a new ingredient row by cloning the existing one
+      const newRow = ingredientRow.cloneNode(true);
+  
+      // Clear the values of the cloned row's inputs
+      newRow.querySelector("#ingredient").value = "";
+      newRow.querySelector("#quantity").value = "";
+      newRow.querySelector("#unit").value = "";
+  
+      // Add the new row to the form
+      ingredientRow.parentNode.insertBefore(newRow, addRowBtn);
+    });
+  
+    // Add event listener to "Remove Row" buttons
+    const removeRowBtns = document.querySelectorAll(".remove-row-btn");
+    removeRowBtns.forEach(btn => {
+      btn.addEventListener("click", () => {
+        btn.parentNode.remove();
+      });
+    });
+
+    //Doesnt fully work, sometimes makes the whole screen empty. When it does works, adding ingredient prints itself twice and the delete button doesnt work
+
+  return (
+    
+<div className="form-content">
+  <h1>Available Dishes in the Menu</h1>
+  <br/><br/>
+  
+  <div className="create-dish-form">
+    <form/>
+      <div className="form-group">
+        <label for="dish-name">Dish Name:</label>
+        <input type="text" id="dish-name" name="dish-name" required />
+        <br/><br/>
+      </div>
+      <div className="form-group">
+        <label for="dish-price">Dish Price:</label>
+        <input type="number" id="dish-price" name="dish-price" step="0.01" min="0" required />
+        <br/>
+      </div>
+      <hr/>
+      <h4>Ingredients:</h4>
+      <div className="ingredient-row">
+        <div className="form-group">
+          <label for="ingredient">Ingredient:</label>
+          <select id="ingredient" name="ingredient">
+            <option value="tomatoes">Tomatoes</option>
+            <option value="onions">Onions</option>
+            <option value="lettuce">Lettuce</option>
+          </select>
+        </div>
+        <div className="form-group">
+          <label for="quantity">Quantity:</label>
+          <input type="number" id="quantity" name="quantity" step="0.01" min="0" required />
+        </div>
+        <div className="form-group">
+          <label for="unit">Unit:</label>
+          <select id="unit" name="unit">
+            <option value="g">g</option>
+            <option value="kg">kg</option>
+            <option value="ml">ml</option>
+            <option value="L">L</option>
+          </select>
+        </div>
+        <button className="remove-row-btn">Remove</button>
+      </div>
+      <button className="add-row-btn">Add Ingredient</button>
+      <hr/>
+      <button type="submit">Create Dish</button>
+    <form/>
+  </div>
+  
+</div>
+
+
+  )
+}
+
+export default MANAGER_addDish
